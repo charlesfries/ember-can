5ec9e5f821cb032465effe82fcd099392284c382
Charles Fries <charliefries@icloud.com>
2024-04-10 17:35:11 -0700

    Update Ember to 4.8.1

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,18 +7,18 @@ module.exports = async function () {
   return {
     scenarios: [
       {
-        name: 'ember-lts-3.20',
+        name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
-            'ember-source': '~3.20.5',
+            'ember-source': '~3.28.0',
           },
         },
       },
       {
-        name: 'ember-lts-3.24',
+        name: 'ember-lts-4.4',
         npm: {
           devDependencies: {
-            'ember-source': '~3.24.3',
+            'ember-source': '~4.4.0',
           },
         },
       },
